9737c45a85508255a049e0b32c0775faff6ea97d
Jeroen de Wolf <wolfjde@hotmail.com>
2019-04-25 10:21:51 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,5 +1,10 @@
 import app
+import unittest
 
 class TestApp:
   def test_hello(self):
-    assert 'Hello World!' == app.hello()
+    rv=self.app.get('/')
+    self.assertEqual(rv.data, b'Hello World!')
+ 
+if __name__ == '__main__':
+  unitest.main()
