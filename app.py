c53af916eb4efa2b8a2d171a0ab41f84c9580fc2
Jeroen de Wolf <wolfjde@hotmail.com>
2019-04-25 10:13:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ redis = Redis(host='redis', port=6379)
 def hello():
 
     redis.incr('hits')
-      return 'Hello World!'
+    return 'Hello World!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
