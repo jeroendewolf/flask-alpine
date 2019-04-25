b92485264317439c19582ff1d1bae1e324f87595
Jeroen de Wolf <wolfjde@hotmail.com>
2019-04-25 17:24:01 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,16 +2,10 @@
 import unittest
 from app import hello
 
-
 class TestApp(unittest.TestCase):
   
-  def setUp(self):
-    app.app.Testing=True
-    self.app=app.app.test_client()
-    
   def test_hello(self):
-    rv=self.app.get('/')
-    self.assertEqual(rv.data, b'Hello World')
+    self.assertTrue(hello() == "Hello World!")
  
 if __name__ == '__main__':
   unittest.main()
