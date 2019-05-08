3d8bc96f2c73a958c9ecf0fe80ea3f5048b4c94d
Jeroen de Wolf <wolfjde@hotmail.com>
2019-05-08 15:54:06 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -12,7 +12,7 @@ class TestApp(unittest.TestCase):
     
   def test_hello_serv(self):
     rv = self.app.get('/')
-    self.assertEqual(rv.status, '200-OK')
+    self.assertEqual(rv.status, '200 OK')
     self.assertEqual(rv.data, b'Hello World!\n')
  
 if __name__ == '__main__':
