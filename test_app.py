4bcff7d2ba83741d691edc522614c53dadda7bda
Jeroen de Wolf <wolfjde@hotmail.com>
2019-05-08 15:52:46 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 import unittest
-from app import hello
+import app
 
 class TestApp(unittest.TestCase):
+  def setUp(self):
+    app.app.testing=True
+    self.app = app.app.test_client()
   
   def test_hello(self):
     self.assertTrue(hello() == "Hello World!")
+    
+  def test_hello_serv(self):
+    rv = self.app.get('/')
+    self.assertEqual(rv.status, '200-OK')
+    self.assertEqual(rv.data, b'Hello World!\n')
  
 if __name__ == '__main__':
   unittest.main()
