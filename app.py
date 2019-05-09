60608f1ba826e43f9c3dac902d32da002b5929ce
Jeroen de Wolf <wolfjde@hotmail.com>
2019-05-09 19:56:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 from flask import Flask
-# from redis import Redis
 
 app = Flask(__name__)
-
-# redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
 
-    # redis.incr('hits')
     return 'Hello World!'
 
 if __name__ == "__main__":
