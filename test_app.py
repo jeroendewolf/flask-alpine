3ecb9df390835a88f476df60803ad7109bc51e3d
Jeroen de Wolf <wolfjde@hotmail.com>
2019-12-10 14:05:10 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,15 +8,15 @@ class TestApp(unittest.TestCase):
   def setUp(self):
     app.app.testing=True
     self.app = app.app.test_client()
-  
-//  def test_hello(self):
-//    self.assertTrue(hello() == "Hello World!")
+ ''' 
+  def test_hello(self):
+    self.assertTrue(hello() == "Hello World!")
     
- // def test_hello_serv(self):
- //   rv = self.app.get('/')
- //   self.assertEqual(rv.status, '200 OK')
- //   self.assertEqual(rv.data, b'Hello World!')
- 
+  def test_hello_serv(self):
+    rv = self.app.get('/')
+    self.assertEqual(rv.status, '200 OK')
+    self.assertEqual(rv.data, b'Hello World!')
+ '''
 if __name__ == '__main__':
   import xmlrunner
   runner = xmlrunner.XMLTestRunner(output='reports')
