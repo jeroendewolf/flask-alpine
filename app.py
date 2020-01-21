712196da4113a717566d57e6d0bab4a2902280e7
Jeroen de Wolf <wolfjde@hotmail.com>
2020-01-21 12:10:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask import Response
+from flask import jsonify
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 
-    xml = 'foo'
-    return Response(xml, mimetype='text/xml')
+    jsonmsg = 'foo bar'
+    return jsonify(jsonmsg)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
