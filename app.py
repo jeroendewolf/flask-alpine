2f28c132f065ec462ec552271cff4ee7fc84eb59
Jeroen de Wolf <wolfjde@hotmail.com>
2020-01-21 21:01:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def helloJson2():
 def jpg_to_pdf():
     pdf = FPDF()
     pdf.add_page()
-    pdf.image('wolf.jpg'), 150, 150)
+    pdf.image('wolf.jpg', 150, 150)
     response = make_response(pdf.output(dest='S').encode('latin-1'))
     response.headers.set('Content-Disposition', 'attachment', filename=name + '.pdf')
     response.headers.set('Content-Type', 'application/pdf')
