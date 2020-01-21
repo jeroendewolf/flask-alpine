ee991eab6f23a545d4595d57093b55f4cac5c124
Jeroen de Wolf <wolfjde@hotmail.com>
2020-01-21 15:41:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,21 +12,21 @@ def helloJson1():
 @app.route('/xml')
 def helloXml():
     xmlMsg = "Hello XML!"
-    r = make_response( xmlMsg )
+    r = flask.make_response( xmlMsg )
     r.mimetype = 'application/xml'
     return r
 
 @app.route('/html')
 def helloHtml():
     htmlMsg = "<html><head></head><body>Hello HTML!</body></html>"
-    r = make_response( htmlMsg )
+    r = flask.make_response( htmlMsg )
     r.mimetype = 'text/html'
     return r
 
 @app.route('/json2')
 def helloJson2():
     jsonMsg = "Hello JSON2!"
-    r = make_response( jsonMsg )
+    r = flask.make_response( jsonMsg )
     r.mimetype = 'application/json'
     return r
 
