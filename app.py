d5979b64594eb5de0d021f1983e32b0ec7fae9b0
Jeroen de Wolf <wolfjde@hotmail.com>
2020-03-05 12:51:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 
-    //return 'Hello World!'
-    xml = 'foo'
-    return Response(xml, mimetype='text/xml')
+    return 'Hello World!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
