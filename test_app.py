dafb52ce65bc1d2af4467a62366c4951ff27911b
Jeroen de Wolf <wolfjde@hotmail.com>
2020-09-17 12:01:06 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -10,12 +10,12 @@ class TestApp(unittest.TestCase):
     self.app = app.app.test_client()
 
   def test_hello(self):
-    self.assertTrue(hello() == "Hello World!")
+    self.assertTrue(hello() == "Hello Keylane!")
 
   def test_hello_serv(self):
     rv = self.app.get('/')
     self.assertEqual(rv.status, '200 OK')
-    self.assertEqual(rv.data, b'Hello World!')
+    self.assertEqual(rv.data, b'Hello Keylane!')
 
 if __name__ == '__main__':
   import xmlrunner
