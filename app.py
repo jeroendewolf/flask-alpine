827c658330a4fd6f2173f09d26253a059d5417a1
Jeroen de Wolf <wolfjde@hotmail.com>
2021-04-02 01:07:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import Response
 
 app = Flask(__name__)
+ip = '0.0.0.0'
 
 @app.route('/')
 def hello():
@@ -9,4 +10,4 @@ def hello():
     return 'Hello KPN!'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host=ip, debug=False)
